5fb3af857295830e8ea7dfcb26e21e46dd53f4d8
Andrey Volkov <andrey.volkov@lamoda.ru>
2016-02-04 00:37:55 +0300

    Bump version 0.1.0.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "doan",
-    version = "0.1.0.dev0",
+    version = "0.1.0",
     packages = find_packages(),
     # metadata for upload to PyPI
     author = "Andrey Volkov",
