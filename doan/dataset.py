a6998fae778252a5183690c7fbe61c60c7a9d9dc
Andrey Volkov <andrey.volkov@lamoda.ru>
2016-02-06 00:47:22 +0300

    LinesIterator for dataset.

--- doan/dataset.py
+++ doan/dataset.py
@@ -5,15 +5,33 @@ from subprocess import check_call
 
 
 class Dataset(object):
-    types_map = {'num': ['i', 'f']}
+
+    """Table with determined columns data type.
+
+    Features:
+    - Data reading with respect to column type.
+    - Columns iterator.
+    - Getting column with predifined type.
+    """
+
+    TYPES = {'f': lambda v: float(v)}
+    TYPES_MAP = {'num': ['i', 'f']}
+
     def __init__(self, name):
-        self.name = name
+        self.name = ''
         self.rows = []
         column_types = []
         self.length = 0
 
     def __len__(self):
         return self.length
+
+    def parse_line(self, line):
+        row = [self.parse_value(v, i) for i, v in enumerate(line.split())]
+        self.add_row(row)
+
+    def parse_value(self, v, i):
+        return self.TYPES[self.column_types[i]](v)
 
     def add_row(self, row):
         self.rows.append(row)
@@ -27,7 +45,7 @@ class Dataset(object):
             yield itemgetter(*args)(row)
 
     def num_column(self):
-        column = list(filter(lambda i: i in self.types_map['num'],
+        column = list(filter(lambda i: i in self.TYPES_MAP['num'],
                              self.column_types))
         if len(column) != 1:
             raise ValueError('Can not find single num column')
@@ -70,12 +88,38 @@ def ssh(host):
     return wrapped
 
 
+class LinesIterator:
+    """Iterate file or any iterable object. """
+
+    def __init__(self, obj):
+        self.obj = obj
+        self.is_file = isinstance(obj, str)
+        self.name = 'iterator' if not self.is_file else obj
+
+    def __iter__(self):
+        if self.is_file:
+            with open(self.obj) as it:
+                for i in lines(it):
+                    yield i
+        else:
+            for i in lines(self.obj):
+                yield i
+
+
 def r_num(obj):
     """Read list of numbers."""
-    it = _get_iterator(obj)
-    dataset = Dataset(it.doan_dataset_name)
+    dataset = Dataset('test')
     dataset.column_types = ['f']
-    for line in lines(it):
-        # TODO move logic to dataset
-        dataset.add_row([float(line)])
+    it = LinesIterator(obj)
+    if it.name:
+        dataset.name = it.name
+
+    for line in it:
+        dataset.parse_line(line)
+
     return dataset
+
+
+# def r_dvn(obj):
+#     """Read date-value-name table."""
+#     pass
--- doan/test.py
+++ doan/test.py
@@ -1,12 +1,13 @@
 import io
 import unittest
-from doan.dataset import Dataset, _get_iterator, r_num
+from doan.dataset import Dataset, LinesIterator, r_num
 from doan.util import chunk, fixed_width, num_list_equal
 from doan.stat import mean, stat
 
 
 class DatasetTest(unittest.TestCase):
     def test_dataset_iterator(self):
+        """Dataset must correctly work as iterable object."""
         dts = Dataset('test_dataset')
         N = 10
         for i in range(N):
@@ -14,26 +15,37 @@ class DatasetTest(unittest.TestCase):
         for i in range(2):
             self.assertEqual(N, len([i for i in dts]))
 
-    def test_column(self):
+    def test_get_dataset_columns(self):
+        """Test column splitting correctness.
+
+        | 0 | 1 | 2 |
+        | 3 | 4 | 5 |
+        """
         dts = Dataset('test_dataset')
         [dts.add_row(i) for i in chunk(range(6), 3)]
-        self.assertEqual([0, 3], list(dts.column(0)))
+        for i in range(3):
+            self.assertEqual([i, i + 3], list(dts.column(i)))
+        # test multiple
+        self.assertEqual([(0, 1), (3, 4)], list(dts.column(0,1)))
         self.assertEqual([(1, 2), (4, 5)], list(dts.column(1,2)))
 
 
 class ReaderTest(unittest.TestCase):
-    def test_get_iterator(self):
-        N = 10
-        s = '1\n' * N
-        sio = io.StringIO(s)
-        sio.doan_dataset_name = '10 lines of 1'
-        self.assertEqual(N, len([i for i in _get_iterator(sio)]))
-        self.assertEqual(sio.doan_dataset_name, _get_iterator(sio).doan_dataset_name)
+
+    def setUp(self):
+        self.N = 10
+        self.str = '1\n' * self.N
+
+    def test_lines_iterator_stringio(self):
+        sio = io.StringIO(self.str)
+        self.assertEqual(self.N, len([i for i in LinesIterator(sio)]))
+
+    def test_lines_iterator_file(self):
         fname = '/tmp/doan-test-generator'
         with open(fname, 'w') as f:
-            f.write(s)
-        self.assertEqual(N, len([i for i in _get_iterator(fname)]))
-        self.assertEqual(fname, _get_iterator(fname).doan_dataset_name)
+            f.write(self.str)
+        self.assertEqual(self.N, len([i for i in LinesIterator(fname)]))
+        self.assertEqual(fname, LinesIterator(fname).name)
 
 
 class IntegrationTest(unittest.TestCase):
