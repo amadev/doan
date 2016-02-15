3c75190012c5a8442ddce32ce0f30a75dd60fd74
Andrey Volkov <andrey.volkov@lamoda.ru>
2016-02-15 23:51:00 +0300

    Setup.py requirements and classifiers.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "doan",
-    version = "0.1.0.dev0",
+    version = "0.2.0.dev0",
     packages = find_packages(),
     # metadata for upload to PyPI
     author = "Andrey Volkov",
@@ -12,4 +12,18 @@ setup(
     license = "MIT",
     keywords = "analytics statistics",
     url = "https://github.com/amadev/doan",
+    install_requires=[
+        'matplotlib>=1.5.1',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities',
+        'Topic :: Scientific/Engineering :: Information Analysis',
+    ],
 )
